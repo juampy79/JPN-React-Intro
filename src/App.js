3bad76defa5d8364af822a377513df098acea310
jnavas <jnavas@veritran.com>
2023-09-08 13:01:57 -0300

    Clase 13 - Local Storage3

--- src/App.js
+++ src/App.js
@@ -7,16 +7,27 @@ import {TodoList} from "./components/TodoList";
 import {TodoButton} from "./components/TodoButton";
 import {TodoItem} from "./components/TodoItem";
 
+const localStorageTODOsList = localStorage.getItem('TODOs_1');
 
-const todoList = [
-    {key: 1, text: "Empezar el curso React", completed: true},
-    {key: 2, text: "Entender el curso React", completed: false},
-    {key: 3, text: "Hacer la web Flyka", completed: false},
-    {key: 4, text: "Aprobar el curso React", completed: false}
-];
+function useLocalStorage(){
+
+}
+
+function setLocalStorage(newTODOs) {
+    localStorage.setItem('TODOs_1', JSON.stringify(newTODOs));
+}
+
+let parsedTODOsList;
+if (!localStorageTODOsList) {
+    setLocalStorage([]);
+    parsedTODOsList = []
+
+} else {
+    parsedTODOsList = JSON.parse(localStorageTODOsList);
+}
 
 function App() {
-    const [TODOs, setTODOs] = React.useState(todoList);
+    const [TODOs, setTODOs] = React.useState(parsedTODOsList);
     const [searchValue, setSearchValue] = React.useState("");
     const totalTODOs = TODOs.length;
     const completedTODOs = TODOs.filter(todo => todo.completed).length;
@@ -25,23 +36,30 @@ function App() {
     );
 
 
+
+
+    const saveTODOs = (newTODOs) => {
+        setLocalStorage(newTODOs);
+        setTODOs(newTODOs)
+    }
+
     const onCompleteTODO = (todoKey) => {
         console.log("Check " + todoKey);
         const updatedTODOs = [...TODOs];
         const index = getIndex(updatedTODOs, todoKey)
         updatedTODOs[index].completed = !updatedTODOs[index].completed
-        setTODOs(updatedTODOs);
+        saveTODOs(updatedTODOs);
     };
 
     function getIndex(updatedTODOs, todoKey) {
         return updatedTODOs.findIndex(item => item.key == todoKey);
     }
 
-    const onDeleteTODO  = (todoKey) => {
+    const onDeleteTODO = (todoKey) => {
         console.log("Delete " + todoKey);
         const updatedTODOs = [...TODOs];
         updatedTODOs.splice(getIndex(updatedTODOs, todoKey), 1)
-        setTODOs(updatedTODOs);
+        saveTODOs(updatedTODOs);
     };
 
     return (
@@ -60,8 +78,8 @@ function App() {
                         <TodoItem key={item.key}
                                   text={item.text}
                                   completed={item.completed}
-                                  onComplete={ () => onCompleteTODO(item.key)}
-                                  onDelete={ () => onDeleteTODO(item.key)}
+                                  onComplete={() => onCompleteTODO(item.key)}
+                                  onDelete={() => onDeleteTODO(item.key)}
                         />
                     )
                 )}
--- src/components/Icon.js
+++ src/components/Icon.js
@@ -2,16 +2,13 @@ import React from "react";
 import "../styles/Icon.css";
 import {ReactComponent as CheckSVG} from "../resources/icons/icon-check.svg";
 import {ReactComponent as DeleteSVG} from "../resources/icons/icon-delete.svg";
-// import { FaCheck as CheckSVG } from 'react-icons/fa';
-// import { MdOutlineDeleteForever as DeleteSVG } from 'react-icons/md';
+
 function Icon({type, color, onClick}) {
 
     const iconTypes = {
         "check": (color) => <CheckSVG fill={color}/>,
         "delete":(color) => <DeleteSVG fill={color}/>
     };
-
-    console.log(`Icon type: ${type}  color: ${color}`);
 
     return (
         <span
